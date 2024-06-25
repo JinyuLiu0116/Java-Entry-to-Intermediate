d00516f30da9cb8dd0bf4cbda7ca8505461fc5ac
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2024-06-25 19:45:43 -0400

    Add files via upload

---
+++ Selection.java
@@ -0,0 +1,34 @@
+package com.mac190.selection;
+//The condition needs to evaluate to true or false.
+//Write a program that converts from celcious to fahrenheit and
+//from fahrenheit to celcius depending on user's choice
+//first ask the user which conversion wishes to make
+//then ask for the temperature and make the appropriate conversion
+import java.util.Scanner;
+public class Selection {
+    public static void main(String[] args) {
+        Scanner keyb = new Scanner(System.in);
+        int choice;
+        double tempert;
+       // boolean choice;
+        System.out.print("Enter 1: converse celcious to fahrenheit. Enter 2: converse fahrenheit to celcious.");
+        choice = keyb.nextInt();
+        System.out.println("Enter the temperature: ");
+        tempert = keyb.nextDouble();
+        if(choice == 1)
+        {
+            tempert = ((tempert*9)/5)+32;
+            System.out.println("The temperature is: "+tempert+" fahrenheit degree.");
+        }
+        else if(choice == 2){
+            tempert = ((tempert-32)*5)/9;
+            System.out.println("The temperature is: "+tempert+" celcious degree.");
+        }
+        else
+        {
+            System.out.println("Invalid input number.");
+        }
+
+
+    }
+}
