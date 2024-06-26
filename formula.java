31f018b07e1257a243852546ad1be77c9cc8e0eb
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2024-06-25 20:59:24 -0400

    Update formula.java

--- formula.java
+++ formula.java
@@ -5,18 +5,18 @@ public class formula {
         Scanner keyb = new Scanner(System.in);
 
         int N;
-        double e,f1,f2,f3,f4,formula;
+        double e;
 
         System.out.print("Enter a integer number:");
         N = keyb.nextInt();
         System.out.print("Enter a dismal number:");
         e = keyb.nextDouble();
 
-        f1 = Math.pow(N,4)-3;
-        f2 = 1-Math.sin(e);
-        f3 = Math.pow(f2,2);
-        f4 = f1/f3;
-        formula = Math.pow(f4,(1/5.0));
+        double f1 = Math.pow(N,4)-3;
+        double f2 = 1-Math.sin(e);
+        double f3 = Math.pow(f2,2);
+        double f4 = f1/f3;
+        double formula = Math.pow(f4,(1/5.0));
         System.out.println("After following a complicated formula, the result is: "+formula);
 
 
