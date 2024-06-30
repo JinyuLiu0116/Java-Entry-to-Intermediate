d9d5b54266b5194589cc04721da1aeaa058f5c31
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2024-06-30 13:13:45 -0400

    Update ArrayDiamondPattern.java

--- ArrayDiamondPattern.java
+++ ArrayDiamondPattern.java
@@ -7,33 +7,22 @@ public class ArrayDiamondPattern {
         int num = keyb.nextInt();
 
         if(num % 2 ==0){
-            num = num -1;
+            num --;
         }
         for(int i=1; i<=num; i+=2)
         {
             for(int n=num-i;n>0;n-=2)
-            {
                 System.out.print(" ");
-            }
             for(int j=1; j<=i; j++)
-            {
                 System.out.print("*");
-            }
             System.out.println();
         }
-        int spaceCouter=1;
         for(int i=3; i<=num; i+=2)
         {
-            for(int n=1;n<=spaceCouter;n++)
-            {
+            for(int n=1;n<=i-2;n+=2)
                 System.out.print(" ");
-
-            }
-            spaceCouter++;
             for(int j=i; j<=num; j++)
-            {
                 System.out.print("*");
-            }
             System.out.println();
         }
     }
