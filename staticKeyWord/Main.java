2d8d404bbae669c93659e04ca2e22ff446041719
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2024-07-10 22:20:10 -0400

    Add files via upload

---
+++ staticKeyWord/Main.java
@@ -0,0 +1,25 @@
+package com.mac190.oopPractice.staticKeyWord;
+
+public class Main {
+    public static void main(String[] args){
+
+        Friend friend1 = new Friend("Sponegbob");
+
+        System.out.println(Friend.numberOfFriends);
+
+        Friend friend2 = new Friend("Patrick");
+
+        System.out.println(Friend.numberOfFriends);
+
+        Friend friend3 = new Friend("Patrick");
+
+        System.out.println(Friend.numberOfFriends);
+
+        Friend.displayFriends();
+
+
+
+
+
+    }
+}
