df6597f81a94df555342fba5869766e1123cbd8f
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2024-07-27 16:56:37 -0400

    Add files via upload

---
+++ rectangle_cube_inheritance/Rectangle.java
@@ -0,0 +1,77 @@
+package com.mac190.rectangle_cube;
+
+public class Rectangle {
+    private double length, width;
+
+    public Rectangle() {
+        this.length = 0;
+        this.width = 0;
+    }
+    public Rectangle(double a, double b) {
+        a = Math.abs(a);
+        b = Math.abs(b);
+        if (a >= b) {
+            this.length = a;
+            this.width = b;
+        } else {
+            this.length = b;
+            this.width = a;
+        }
+    }
+    public Rectangle(Rectangle r) {
+        this.length = r.getLength();
+        this.width = r.getWidth();
+    }
+    public double getLength() {
+        return this.length;
+    }
+    public double getWidth() {
+        return this.width;
+    }
+    public void setLength(double a) {
+        a = Math.abs(a);
+        if (this.getWidth() == a)
+            this.length = a;
+        if (a < this.getWidth()) {
+            this.length = this.getWidth();
+            this.width = a;
+        } else
+            this.length = a;
+    }
+    public void setWidth(double a) {
+        a = Math.abs(a);
+        if (this.getLength() == a)
+            this.width = a;
+        if (a > this.getLength()) {
+            this.width = this.getLength();
+            this.length = a;
+        } else
+            this.width = a;
+    }
+    public double area() {
+        return this.getLength() * this.getWidth();
+    }
+    public double Perm() {
+        return (this.getLength() + this.getWidth()) * 2;
+    }
+    public String toString() {
+        return "(L:" + this.getLength() + ", W:" + this.getWidth() + ")";
+    }
+    public boolean equals(Object obj) {
+        if (obj == null) return false;
+        if (this == obj) return true;
+        if (!(obj instanceof Rectangle)) return false;
+        Rectangle r = (Rectangle) obj;
+        return this.getLength() == r.getLength() && this.getWidth() == r.getWidth();
+    }
+    public Rectangle add(Rectangle r) {
+        return new Rectangle(this.getLength() + r.getLength(), this.getWidth() + r.getWidth());
+    }
+    //method add() that accepts a double and returns a Rectangle with all sides augmented/reduced by the input. all side shoud remain positive
+    public Rectangle add(double a) {
+        return new Rectangle(Math.abs(this.getLength() - a), Math.abs(this.getWidth() - a));
+    }
+    public Rectangle multiply(double a) {
+        return new Rectangle(this.getLength() * Math.abs(a), this.getWidth() * Math.abs(a));
+    }
+}
