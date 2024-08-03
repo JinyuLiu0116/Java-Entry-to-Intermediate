25b58629f2b84f7e720192b0cc6206c7be7e8cae
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2024-08-03 12:53:01 -0400

    Delete Stock_trading directory

--- Stock_trading/iBroker.java
+++
@@ -1,9 +0,0 @@
-package com.mac190.stocks_abstract_exception;
-
-import java.util.ArrayList;
-
-public interface iBroker {
-    ArrayList<Position> getHoldings() ;
-    public void buy(String ticker, String type, int shares, float price) throws Exception;
-    double sell(String ticker, String type, int shares, float price) throws Exception;
-}
